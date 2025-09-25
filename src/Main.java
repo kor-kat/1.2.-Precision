7320da6c33b80254d754b97880a7be075d8dc540
Kate Korobova <korkat1611@gmail.com>
2025-09-25 13:13:59 +0300

    changing data type

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        double regularBonus = 0.9;
-        double specialBonus = 0.6;
-        double totalBonus = regularBonus + specialBonus;
+        float regularBonus = 0.3F;
+        float specialBonus = 0.6F;
+        float totalBonus = regularBonus + specialBonus;
         System.out.println(totalBonus);
     }
 }
