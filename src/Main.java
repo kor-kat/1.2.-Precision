f514dc779553917a2c6ce3ac36d95718b34cb061
Kate Korobova <korkat1611@gmail.com>
2025-09-25 13:13:59 +0300

    bug fix

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        float regularBonus = 0.3F;
-        float specialBonus = 0.6F;
-        float totalBonus = regularBonus + specialBonus;
+        double regularBonus = 0.9;
+        double specialBonus = 0.6;
+        double totalBonus = regularBonus + specialBonus;
         System.out.println(totalBonus);
     }
 }
